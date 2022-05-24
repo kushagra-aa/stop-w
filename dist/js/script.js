a1761bca50539b7a7ff48b16bc8bce756953338b
kushagra-aa <kushagraagnihotri24@gmail.com>
2022-05-24 15:16:39 +0530

    styled

---
+++ dist/js/script.js
@@ -0,0 +1,9 @@
+// ELEMENTS
+const timeElm = document.querySelector("[data-time]");
+// buttons
+const buttonsElm = document.querySelectorAll("[data-button]");
+// laps container
+const lapContainerElm = document.querySelector("[data-laps]");
+console.log("timeElm :>> ", timeElm);
+console.log("buttonsElm :>> ", buttonsElm);
+console.log("lapContainerElm :>> ", lapContainerElm);
